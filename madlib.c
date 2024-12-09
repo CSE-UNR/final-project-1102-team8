8daca16b8ef5409633672a3e393bfacd1e7918ef
1102-Sarellano-Luis <luisangelsarellanolopez@gmail.com>
2024-12-09 18:21:31 +0000

    joe

---
+++ madlib.c
@@ -0,0 +1,30 @@
+//Luka Wozniak and Luis Sarellano
+//12-5-24
+//Make a Madlib
+
+#include<stdio.h>
+void choose(); //Choose a madlib from the repository at random
+void count(); //Count verbs, adjectives, and nouns
+void userint(); //Does the prompting and storing of user inputs
+void match(); //matches the inputs to the correct spot in the madlib
+void display(); //displays the updated madlib
+
+	int main(){
+	
+	return 0;
+	}
+void choose(){
+
+}
+void count(){
+
+}
+void userint(){
+
+}
+void match(){
+
+}
+void display(){
+
+}
